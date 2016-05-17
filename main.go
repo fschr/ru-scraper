9d0f99850609fb44d82ba5f926848e4781436f2e
Thomas Fischer <tdf.tomfischer@gmail.com>
2016-05-17 14:44:11 -0500

    Add super hacky section support. Needs to be reorganized. Does not handle any edge cases.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"regexp"
 	"strings"
 )
 
@@ -20,12 +21,102 @@ const (
 		`&titles=`
 )
 
-var word string
+var word, section string
 
 func main() {
 	flag.StringVar(&word, "word", "говорить", "the word to look for")
+	flag.StringVar(&section, "section", "nil", "returns a specified section of the output")
 	flag.Parse()
-	fmt.Println(getRussian(getText(baseURL + word)))
+	russian := getRussian(getText(baseURL + word))
+	var output string
+	switch section {
+	case "nil":
+		output = russian
+	default:
+		output = getSection(russian, section)
+	}
+	fmt.Print(output)
+}
+
+func getSection(russian, s string) string {
+	sectionTitle := "===" + s + "==="
+	sectionTitleLen := len(sectionTitle)
+	scanner := bufio.NewScanner(strings.NewReader(russian))
+	var section string
+	searching := true
+	for scanner.Scan() {
+		line := scanner.Text()
+		if searching {
+			indx := strings.Index(line, sectionTitle)
+			if indx == -1 {
+				continue
+			}
+			searching = false
+			if indx+sectionTitleLen != len(line)-1 {
+				section += line[indx+sectionTitleLen:]
+			}
+		} else {
+			if strings.HasPrefix(line, "[[") || strings.HasPrefix(line, "===") {
+				break
+			}
+			if line == "" || strings.HasPrefix(line, "{{") {
+				continue
+			}
+			if strings.HasPrefix(line, "# ") {
+				line = line[2:]
+				line = strings.Replace(line, "[[", "", -1)
+				line = strings.Replace(line, "]]", "", -1)
+				r := regexp.MustCompile(`\W*{{.*}}\W*`)
+				section += r.ReplaceAllString(line, "") + "\n"
+				continue
+			}
+			if strings.HasPrefix(line, "#: {{ux|ru|") {
+				line = line[11:]
+				line = strings.Replace(line, "'''", "", -1)
+				r := regexp.MustCompile(`([^\|]*)\|`)
+				section += "\tExample: " + r.FindStringSubmatch(line)[1]
+				r = regexp.MustCompile(`\|t=(.*)\|`)
+				section += " — " + r.FindStringSubmatch(line)[1] + "\n"
+				continue
+			}
+			section += line + "\n"
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return section
+}
+
+func getRussian(text string) string {
+	scanner := bufio.NewScanner(strings.NewReader(text))
+	var russian string
+	searching := true
+	for scanner.Scan() {
+		line := scanner.Text()
+		if searching {
+			indx := strings.Index(line, "==Russian==")
+			if indx == -1 {
+				continue
+			}
+			searching = false
+			if indx+len("==Russian==") != len(line)-1 {
+				russian += line[indx+len("==Russian=="):]
+			}
+		} else {
+			otherLang := func(line string) bool {
+				return strings.HasPrefix(line, "==") && !strings.HasPrefix(line, "===")
+			}
+			if strings.HasPrefix(line, "[[") || otherLang(line) {
+				break
+			}
+			russian += line + string('\n')
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return russian
 }
 
 func getText(URL string) string {
@@ -64,35 +155,4 @@ func getText(URL string) string {
 		panic(err)
 	}
 	return text
-}
-
-func getRussian(text string) string {
-	scanner := bufio.NewScanner(strings.NewReader(text))
-	var russian string
-	searching := true
-	for scanner.Scan() {
-		line := scanner.Text()
-		if searching {
-			indx := strings.Index(line, "==Russian==")
-			if indx == -1 {
-				continue
-			}
-			searching = false
-			if indx+len("==Russian==") != len(line)-1 {
-				russian += line[indx+len("==Russian=="):]
-			}
-		} else {
-			otherLang := func(line string) bool {
-				return strings.HasPrefix(line, "==") && !strings.HasPrefix(line, "===")
-			}
-			if strings.HasPrefix(line, "[[") || otherLang(line) {
-				break
-			}
-			russian += line + string('\n')
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
-	return russian
 }
